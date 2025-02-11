bff6f315cd19f701f99d33e02659fd28636be429
AkiKurisu <2683987717@qq.com>
2025-02-11 22:22:34 +0800

    Update ExportConstants.cs

--- Mod/Editor/ExportConstants.cs
+++ Mod/Editor/ExportConstants.cs
@@ -8,4 +8,28 @@ namespace Chris.Mod.Editor
         
         public static string ExportPath => exportPath.GetPath();
     }
+    
+    public class LazyDirectory
+    {
+        private readonly string _path;
+        
+        private bool _initialized;
+        
+        public LazyDirectory(string path)
+        {
+            _path = path;
+        }
+        
+        public string GetPath()
+        {
+            if (!_initialized) return _path;
+            
+            if (!Directory.Exists(_path))
+            {
+                Directory.CreateDirectory(_path);
+            }
+            _initialized = true;
+            return _path;
+        }
+    }
 }
