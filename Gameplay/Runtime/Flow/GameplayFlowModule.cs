1f70de43a1e23bf6f7f7b0da9637d3986fee7b09
AkiKurisu <2683987717@qq.com>
2025-04-10 20:44:13 +0800

    Add runtime module

--- Gameplay/Runtime/Flow/GameplayFlowModule.cs
+++ Gameplay/Runtime/Flow/GameplayFlowModule.cs
@@ -1,15 +1,13 @@
 using Ceres.Graph;
 using Chris.Gameplay.Animations;
-using UnityEngine;
-namespace Chris.Gameplay.Flow.Utilities
+using UnityEngine.Scripting;
+
+namespace Chris.Gameplay.Flow
 {
-    internal static class GameplaySetup
+    [Preserve]
+    internal class GameplayFlowModule: RuntimeModule
     {
-        [RuntimeInitializeOnLoadMethod]
-#if UNITY_EDITOR
-        [UnityEditor.InitializeOnLoadMethod]
-#endif
-        private static void InitializeOnLoad()
+        public override void Initialize(ModuleConfig config)
         {
             /* Register port implicit conversation */
             // ========================= Animation =========================== //
