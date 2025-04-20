b69d3716eba552f20f41c4c18517cfb7616aa4db
AkiKurisu <2683987717@qq.com>
2025-04-20 22:19:05 +0800

    Add actor hot update system

---
+++ Gameplay/Editor/Core/ActorFlowGraphSettingsDrawer.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Ceres.Graph;
+using Ceres.Graph.Flow;
+using Chris.Serialization;
+using UnityEditor;
+using UnityEngine;
+using UDebug = UnityEngine.Debug;
+
+namespace Chris.Gameplay.Editor
+{
+    [CustomPropertyDrawer(typeof(Actor.FlowGraphSettings))]
+    public class ActorFlowGraphSettingsDrawer : PropertyDrawer
+    {
+        private const string FoldoutKey = "Actor_Flow_Advanced_Settings";
+
+        private static class Styles
+        {
+            public static readonly GUIContent ExportRemoteAssetLabel;
+
+            static Styles()
+            {
+                var image = EditorGUIUtility.IconContent("Download-Available@2x").image;
+                ExportRemoteAssetLabel = new GUIContent(image, 
+                    "Export actor flow graph data to a remote asset for hot update");
+            }
+        }
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            Rect foldoutRect = position;
+            foldoutRect.y += 2;
+            foldoutRect.height = EditorGUIUtility.singleLineHeight;
+            Foldout(foldoutRect, FoldoutKey, "Advanced Settings", () =>
+            {
+                position.height = EditorGUIUtility.singleLineHeight;
+                position.y += position.height + 10;
+                EditorGUI.PropertyField(position, property.FindPropertyRelative(nameof(Actor.FlowGraphSettings.graphAsset)));
+                position.y += position.height + 5;
+                float width = position.width;
+                position.width = width - 35;
+                var addressProp = property.FindPropertyRelative(nameof(Actor.FlowGraphSettings.actorAddress));
+                EditorGUI.PropertyField(position, addressProp);
+                position.x += position.width + 5;
+                position.width = 30;
+                using (new EditorGUI.DisabledScope(string.IsNullOrEmpty(addressProp.stringValue)))
+                {
+                    if (GUI.Button(position, Styles.ExportRemoteAssetLabel))
+                    {
+                        var actor = (IFlowGraphContainer)property.serializedObject.targetObject;
+                        ExportRemoteAsset(actor, addressProp.stringValue);
+                    }
+                }
+            }, !Application.isPlaying);
+            EditorGUI.EndProperty();
+        }
+
+        private static void ExportRemoteAsset(IFlowGraphContainer container, string address)
+        {
+            var data = container.GetFlowGraphData();
+            var flowPath = Path.Combine(SaveUtility.SavePath, "Flow");
+            var serializer = new SaveLoadSerializer(flowPath, "bin");
+            if (!ActorFlowGraphDataTableManager.Get().TryGetRemoteUpdatePath(address, out var path))
+            {
+                path = address;
+            }
+            serializer.Save(path, data);
+            UDebug.Log($"Export {container.GetIdentifier()} remote asset to {Path.Combine(flowPath, $"{address}.bin")}");
+            Process.Start(flowPath);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            bool foldOut = EditorPrefs.GetBool(FoldoutKey);
+            if (foldOut)
+            {
+                return EditorGUIUtility.singleLineHeight * 3 + 5 * 2 + 2;
+            }
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        private static void Foldout(
+            Rect rect,
+            string foldKey,
+            string title = null,
+            Action drawAct = null,
+            bool enable = true
+        )
+        {
+            var style = new GUIStyle("ShurikenModuleTitle")
+            {
+                font = new GUIStyle(EditorStyles.label).font,
+                border = new RectOffset(15, 7, 4, 4),
+                fixedHeight = 22,
+                contentOffset = new Vector2(20f, -2f)
+            };
+
+            var color = GUI.backgroundColor;
+            GUI.backgroundColor = Color.white;
+            GUI.Box(rect, title ?? foldKey, style);
+            GUI.backgroundColor = color;
+
+            var e = Event.current;
+            bool foldOut = EditorPrefs.GetBool(foldKey);
+
+            if (e.type == EventType.Repaint)
+            {
+                var arrowRect = new Rect(rect.x + 4f, rect.y + 2f, 13f, 13f);
+                EditorStyles.foldout.Draw(arrowRect, false, false, foldOut, false);
+            }
+
+            if (e.type == EventType.MouseDown && rect.Contains(e.mousePosition))
+            {
+                foldOut = !foldOut;
+                EditorPrefs.SetBool(foldKey, foldOut);
+                e.Use();
+            }
+
+            if (foldOut && drawAct != null)
+            {
+                using (new EditorGUI.IndentLevelScope())
+                {
+                    using (new EditorGUI.DisabledScope(!enable))
+                    {
+                        drawAct();
+                    }
+                }
+            }
+        }
+    }
+}
