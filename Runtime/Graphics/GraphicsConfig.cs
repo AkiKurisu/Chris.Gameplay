e261040a82dd7b08cf867b483418b932e67f3533
AkiKurisu <2683987717@qq.com>
2025-12-06 18:26:50 +0800

    Update GraphicsConfig.cs

--- Runtime/Graphics/GraphicsConfig.cs
+++ Runtime/Graphics/GraphicsConfig.cs
@@ -10,23 +10,18 @@ namespace Chris.Gameplay.Graphics
     [ConfigPath("Chris.Graphics")]
     public class GraphicsConfig: Config<GraphicsConfig>
     {
-        [JsonConverter(typeof(ReactivePropertyConverter<int>))]
         public ReactiveProperty<int> FrameRate { get; set; } = new(0);
         
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
         public ReactiveProperty<bool> Bloom { get; set; } = new(true);
         
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<bool> DepthOfField { get; set; } = new(true);
 #else
         public ReactiveProperty<bool> DepthOfField { get; set; } = new(false);
 #endif
             
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
         public ReactiveProperty<bool> MotionBlur { get; set; } = new(false);
-            
-        [JsonConverter(typeof(ReactivePropertyConverter<int>))]
+        
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<int> RenderScale { get; set; } = new(3);
 #else
@@ -36,41 +31,34 @@ namespace Chris.Gameplay.Graphics
         [JsonIgnore]
         public static readonly float[] RenderScalePresets = { 0.7f, 0.8f, 0.9f, 1.0f };
         
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
         public ReactiveProperty<bool> Vignette { get; set; } = new(true);
         
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<bool> ContactShadows { get; set; } = new(true);
 #else
         public ReactiveProperty<bool> ContactShadows { get; set; } = new(false);
 #endif
             
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<bool> PercentageCloserSoftShadows { get; set; } = new(true);
 #else
         public ReactiveProperty<bool> PercentageCloserSoftShadows { get; set; } = new(false);
 #endif
-                        
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
-        public ReactiveProperty<bool> ScreenSpaceAmbientOcclusion { get; set; } = new(true);
             
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
+        public ReactiveProperty<bool> ScreenSpaceAmbientOcclusion { get; set; } = new(true);
+        
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<bool> ScreenSpaceReflection { get; set; } = new(true);
 #else
         public ReactiveProperty<bool> ScreenSpaceReflection { get; set; } = new(false);
 #endif
             
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
 #if UNITY_STANDALONE_WIN
         public ReactiveProperty<bool> ScreenSpaceGlobalIllumination { get; set; } = new(true);
 #else
         public ReactiveProperty<bool> ScreenSpaceGlobalIllumination { get; set; } = new(false);
 #endif
             
-        [JsonConverter(typeof(ReactivePropertyConverter<bool>))]
         public ReactiveProperty<bool> VolumetricFog { get; set; } = new(true);
         
         [JsonProperty]
